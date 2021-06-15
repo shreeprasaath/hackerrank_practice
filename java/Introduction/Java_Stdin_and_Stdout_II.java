44f1077b3fc07cfe91d90fda56b2e2beee9c3fc7
SP <63418491+shreeprasaath@users.noreply.github.com>
2021-06-15 09:25:17 +0530

    Create Java_Stdin_and_Stdout_II.java

---
+++ java/Introduction/Java_Stdin_and_Stdout_II.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int i = scanner.nextInt();
+        
+        double j = scanner.nextDouble();
+        
+        scanner.nextLine();
+        
+        String k = scanner.nextLine();
+        
+        scanner.close();
+        
+        System.out.println("String: " + k);
+        System.out.println("Double: " + j);
+        System.out.println("Int: " + i);
+        
+    }
+}
